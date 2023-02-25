88bac38e63c1e8c04de5e99b726e69c396dd625f
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-02-25 13:53:01 +0900

    🚀 feat: スタンプラリー／スポット が 追加／更新 された際に経路算出コマンドを発行するようにした
    #1

---
+++ command-firestore-agent.js
@@ -0,0 +1,32 @@
+// スタンプラリーコマンド定数群
+const COMMAND_ENTRY_STAMP_RALLY = 'enterStampRally'         // 参加
+const COMMAND_COMPLETE_STAMP_RALLY = 'completeStampRally'   // 完了
+const COMMAND_WITHDROW_STAMP_RALLY = 'withdrawStampRally'   // 中断
+const COMMAND_CALCULATE_ROUTE = 'calculateRoute';           // 経路算出
+
+// Firestoreを操作するクラス
+class CommandFirestoreAgent {
+    constructor(firebaseAuth, commandCollectionName) {
+      // FirestoreAppを生成する
+      this.firestore = FirestoreApp.getFirestore(firebaseAuth.email, firebaseAuth.key, firebaseAuth.projectId);
+  
+      // コレクション名
+      this.commandCollectionName = commandCollectionName;
+    }
+  
+    // 経路算出コマンドドキュメントを保存する
+    createCalculateRouteCommandDocuments(stampRallies) {
+      for (const id in stampRallies) {
+        const data = {
+            commandType: COMMAND_CALCULATE_ROUTE,
+            data: {stampRallyId : id},
+            uid: 'admin',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+        const path = this.commandCollectionName + '/' + id;
+        // commandコレクションに経路算出コマンドドキュメントを新規追加
+        this.firestore.addDocument(path, data);
+      }
+    }
+  }
